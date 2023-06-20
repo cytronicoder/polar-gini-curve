895659e7e677095c8f3741980d304e67e0fba8e5
Zeyu Yao <novodoodle@gmail.com>
2023-06-19 20:59:38 -0500

    Add custom CSS (test commit)

--- util/render_markdown.py
+++ util/render_markdown.py
@@ -4,17 +4,24 @@ import markdown.extensions.codehilite
 from pygments.formatters import HtmlFormatter
 
 
-def render_markdown(file_name):
+def load_css(css_file):
+    with open(css_file, "r") as f:
+        return f.read()
+
+
+def render_markdown(file_name, css_file="custom.css"):
     readme_file = open(file_name, "r", encoding="utf-8")
 
     md_template_string = markdown.markdown(
         readme_file.read(), extensions=["fenced_code", "codehilite"]
     )
 
-    # TODO: add custom css to markdown
     formatter = HtmlFormatter(style="emacs", full=True, cssclass="codehilite")
     css_string = formatter.get_style_defs()
     md_css_string = "<style>" + css_string + "</style>"
 
-    md_template = md_css_string + md_template_string
+    custom_css = load_css(css_file)
+    custom_css_string = "<style>" + custom_css + "</style>"
+
+    md_template = md_css_string + custom_css_string + md_template_string
     return md_template
