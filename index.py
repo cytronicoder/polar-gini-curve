caa5fcf95bc7226ba95a3df00e69ba91990f0477
Zeyu Yao <novodoodle@gmail.com>
2023-07-04 23:23:30 -0400

    Allow Vercel to use tmp storage to store plots

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, send_from_directory
 from werkzeug.utils import secure_filename
 
 import matplotlib
-matplotlib.use('Agg')  # Use a non-interactive backend
+
+matplotlib.use("Agg")  # Use a non-interactive backend
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -13,16 +14,6 @@ from util.render_markdown import (
 
 app = Flask(__name__)
 
-# check if folders exist
-if not os.path.exists("uploads"):
-    os.makedirs("uploads")
-
-if not os.path.exists("static/images"):
-    os.makedirs("static/images")
-
-app.config["UPLOAD_FOLDER"] = "uploads/"
-app.config["IMAGE_FOLDER"] = "static/images/"
-
 
 @app.route("/")
 def index():
@@ -32,18 +23,14 @@ def index():
 @app.route("/playground/", methods=["GET", "POST"])
 def playground():
     if request.method == "POST":
-        if 'file' not in request.files:
-            return 'No file part'
-        file = request.files['file']
-        if file.filename == '':
-            return 'No selected file'
+        if "file" not in request.files:
+            return "No file part"
+        file = request.files["file"]
+        if file.filename == "":
+            return "No selected file"
         if file:
             filename = secure_filename(file.filename)
             without_extension = os.path.splitext(filename)[0]
-
-            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            file.save(filepath)
-            print(f"{filename} uploaded!")
 
             # TODO: use the file data and the functions from script to generate plot
             # Below is an example of creating a simple plot
@@ -51,15 +38,20 @@ def playground():
             y = np.sin(x)
             plt.plot(x, y)
 
-            # Save the plot as an image in the "static/images" folder
+            # Save the plot as an image in temporary storage
             filename = without_extension + "_plot.png"
-            plt.savefig(os.path.join(app.config['IMAGE_FOLDER'], filename))
+            plt.savefig(os.path.join("/tmp", filename))
             plt.close()
 
             # Pass image filename to the template to display it
             return render_template("display.html", filename=filename)
 
     return render_template("upload.html")
+
+
+@app.route("/tmp/<path:filename>")
+def serve_tmp_file(filename):
+    return send_from_directory("/tmp", filename)
 
 
 if __name__ == "__main__":
