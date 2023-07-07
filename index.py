aca7107d2f11a32d3db1d97f8140d8f3108e9ee1
Zeyu Yao <novodoodle@gmail.com>
2023-07-06 21:58:27 -0400

    Display t-SNE graphs!!

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, send_from_directory
-from werkzeug.utils import secure_filename
-
-import matplotlib
-
-matplotlib.use("Agg")  # Use a non-interactive backend
-import matplotlib.pyplot as plt
+from flask import (
+    Flask,
+    request,
+    render_template,
+    jsonify,
+    url_for,
+    session,
+    send_from_directory,
+)
+from io import BytesIO
+from scipy.io import loadmat
 
 import numpy as np
 import os
@@ -12,11 +16,16 @@ from util.render_markdown import (
     render_markdown,
 )
 
+from util.graph_tsne import graph_tsne
+
 app = Flask(__name__)
+app.secret_key = "polarginicurves"  # dummy secret key
 
 # Create temporary storage for the plot
 if not os.path.exists("/tmp"):
     os.mkdir("/tmp")
+
+gene_list = set()
 
 
 @app.route("/")
@@ -24,33 +33,105 @@ def index():
     return render_markdown("README.md", "home.html")
 
 
-@app.route("/playground/", methods=["GET", "POST"])
+@app.route("/playground/")
 def playground():
-    if request.method == "POST":
-        if "file" not in request.files:
-            return "No file part"
-        file = request.files["file"]
-        if file.filename == "":
-            return "No selected file"
-        if file:
-            filename = secure_filename(file.filename)
-            without_extension = os.path.splitext(filename)[0]
-
-            # TODO: use the file data and the functions from script to generate plot
-            # Below is an example of creating a simple plot
-            x = np.linspace(0, 10, 100)
-            y = np.sin(x)
-            plt.plot(x, y)
-
-            # Save the plot as an image in temporary storage
-            filename = without_extension + "_plot.png"
-            plt.savefig(os.path.join("/tmp", filename))
-            plt.close()
-
-            # Pass image filename to the template to display it
-            return render_template("display.html", filename=filename)
-
     return render_template("upload.html")
+
+
+@app.route("/process_clusters", methods=["POST"])
+def process_clusters():
+    file = request.files["cluster_id"]
+    if file:
+        file_bytes = BytesIO(file.read())
+        mat_content = loadmat(file_bytes)
+
+        # Assuming the mat file has a variable named 'clusters' with cluster data
+        clusters = mat_content.get("clusterID")
+        if clusters is not None:
+            unique_clusters = np.unique(clusters).tolist()
+            unique_clusters[unique_clusters.index(-1)] = "None"
+            return jsonify({"clusters": unique_clusters})
+
+
+@app.route("/check_gene_marker", methods=["POST"])
+def check_gene_marker():
+    data = request.json
+    gene_marker = data.get("gene_marker")
+
+    # Here, check if gene_marker exists in gene_list
+    exists = gene_marker in gene_list
+
+    print(exists)
+
+    return jsonify({"exists": exists})
+
+
+@app.route("/process_gene_list", methods=["POST"])
+def process_gene_list():
+    if "gene_list" in request.files:
+        file = request.files["gene_list"]
+        if file.filename.endswith(".mat"):
+            file_bytes = BytesIO(file.read())
+            mat = loadmat(file_bytes)
+            global gene_list
+
+            flattened_gene_list = [item[0][0] for item in mat.get("geneList")]
+            gene_list = set(flattened_gene_list)
+            print(gene_list)
+    return (
+        "",
+        204,
+    )
+
+
+@app.route("/draw_tsne", methods=["POST"])
+def draw_tsne():
+    gene_marker = request.form["gene_marker"]
+    selected_cluster = request.form["selected_cluster"]
+
+    session["gene_marker"] = gene_marker
+    session["selected_cluster"] = selected_cluster
+
+    # Handle the uploaded files
+    coordinate_file = request.files["coordinate"]
+    cluster_id_file = request.files["cluster_id"]
+    expression_file = request.files["expression"]
+    gene_list = request.files["gene_list"]
+
+    # Load the files
+    coordinate_file = BytesIO(coordinate_file.read())
+    cluster_id_file = BytesIO(cluster_id_file.read())
+    expression_file = BytesIO(expression_file.read())
+    gene_list = BytesIO(gene_list.read())
+
+    # Read the data from the files (assuming they are .mat files)
+    coordinate = loadmat(coordinate_file)["coordinate"]
+    cluster_id = loadmat(cluster_id_file)["clusterID"]
+    expression = loadmat(expression_file)["Expression"]
+    gene_list = loadmat(gene_list)["geneList"]
+
+    # Call the draw_tsne function to generate the graph
+    graph_tsne(
+        marker_gene=gene_marker,
+        coordinate=coordinate,
+        cluster_id=cluster_id,
+        target_cluster_id=int(selected_cluster),
+        expression_data=expression,
+        gene_list=gene_list,  # This should be loaded previously or you can include it in the form
+        random_state=0,
+    )
+
+    # Render display.html
+    return jsonify(url=url_for("display_tsne"))
+
+
+@app.route("/display_tsne")
+def display_tsne():
+    gene_marker = session.get("gene_marker")
+    selected_cluster = session.get("selected_cluster")
+    return render_template(
+        "display.html", title_for=f"t-SNE for marker gene {gene_marker} in cluster {selected_cluster}", filename=f"tsne_{gene_marker}_c-{selected_cluster}.png"
+    )
 
 
 @app.route("/tmp/<path:filename>")
---
+++ util/graph_tsne.py
@@ -0,0 +1,98 @@
+from sklearn.manifold import TSNE
+
+import matplotlib
+
+matplotlib.use("Agg")  # Use a non-interactive backend
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+
+
+def graph_tsne(
+    marker_gene,
+    coordinate,
+    cluster_id,
+    target_cluster_id,
+    expression_data,
+    gene_list,
+    random_state=0,
+):
+    """
+    Plot t-SNE visualization for a marker gene in a specific cluster.
+
+    Parameters:
+        marker_gene (str): The marker gene to visualize.
+        coordinate (ndarray): The 2D spatial coordinates.
+        cluster_id (ndarray): The cluster ID for each cell.
+        target_cluster_id (int): The ID of the target cluster.
+        expression_data (ndarray): The gene expression data.
+        gene_list (ndarray): The list of gene symbols.
+        random_state (int, optional): Random state for t-SNE. Default is 0.
+
+    Returns:
+        None. The plot is displayed and saved as a PNG file.
+    """
+
+    print(target_cluster_id)
+    print(marker_gene)
+    print(coordinate)
+    print(cluster_id)
+    print(expression_data)
+    print(gene_list)
+    print(random_state)
+
+    # Create a t-SNE model
+    model = TSNE(n_components=2, random_state=random_state)
+
+    # Flatten cluster_id
+    cluster_id = np.ravel(cluster_id)
+
+    # Calculate t-SNE coordinates for all cells
+    tsne_coordinates = model.fit_transform(coordinate)
+
+    # Set up the plot
+    _, ax = plt.subplots()
+
+    # Plot target cluster in grey
+    target_cluster = tsne_coordinates[cluster_id == target_cluster_id]
+    ax.scatter(
+        target_cluster[:, 0], target_cluster[:, 1], color="grey", s=1, alpha=0.25
+    )
+
+    # Find cells in the target cluster that express the marker gene
+    marker_gene_index = np.where(gene_list == marker_gene)[
+        0
+    ]  # find index of marker gene
+    marker_gene_expression = (
+        expression_data[:, marker_gene_index].flatten() > 0
+    )  # find cells that express the marker gene
+
+    print(cluster_id.shape)
+    print(marker_gene_expression.shape)
+
+    marker_gene_in_target_cluster = np.logical_and(
+        cluster_id == target_cluster_id, marker_gene_expression
+    )
+
+    # Plot these cells in red
+    marker_gene_coordinates = tsne_coordinates[marker_gene_in_target_cluster.ravel(), :]
+    ax.scatter(
+        marker_gene_coordinates[:, 0],
+        marker_gene_coordinates[:, 1],
+        color="red",
+        s=1,
+    )
+
+    # Labels and title
+    ax.set_xlabel("t-SNE 1")
+    ax.set_ylabel("t-SNE 2")
+
+    # Save figure in png format
+    file_name = f"tsne_{marker_gene}_c-{target_cluster_id}.png"
+    file_path = os.path.join('tmp', file_name)
+    plt.savefig(file_path)
+    
+    # Optionally, you can still show the plot
+    # plt.show()
