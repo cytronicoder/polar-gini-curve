c100e8d0dc151e643bf29eb9a30ef57dcca6cf63
Zeyu Yao <novodoodle@gmail.com>
2025-08-22 23:31:14 +0800

    Add configuration for setuptools

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import sys, os
+from pathlib import Path
+
+root = Path(__file__).resolve().parents[1]
+src = root / "src"
+src_str = str(src)
+if src_str not in sys.path:
+    sys.path.insert(0, src_str)
+
+if "PYTHONPATH" in os.environ:
+    if src_str not in os.environ["PYTHONPATH"].split(os.pathsep):
+        os.environ["PYTHONPATH"] = src_str + os.pathsep + os.environ["PYTHONPATH"]
+else:
+    os.environ["PYTHONPATH"] = src_str
